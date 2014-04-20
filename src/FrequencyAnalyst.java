3b191a88a2405d8c95e8d078fd8f47d5bed99c92
Stephen "Clay" Hatridge <schatridge@gmail.com>
2014-04-20 11:05:15 -0500

    Enhancements to JSON Creation
    See title

--- src/FrequencyAnalyst.java
+++ src/FrequencyAnalyst.java
@@ -1,9 +1,19 @@
 
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class FrequencyAnalyst {
 	private Map<String, Integer> wordCatalog = new TreeMap<String, Integer>();
+	private boolean activeData = false;
 	
 	public FrequencyAnalyst(String filepath) {
 		readDocument(filepath);
@@ -13,6 +23,17 @@ public class FrequencyAnalyst {
 	
 	public void clear() {
 		wordCatalog.clear();
+		activeData = false;
+	}
+	
+	private String stem(String word) throws Throwable {
+		/*Class stemClass = Class.forName("org.tartarus.snowball.ext. Frequency Analysis Stemmer");
+		SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
+		
+		stemmer.setCurrent(word);
+		stemmer.stem();
+		
+		return stemmer.getCurrent();*/ return word;
 	}
 	
 	public void clearAndReadDocument(String filepath) {
@@ -30,6 +51,13 @@ public class FrequencyAnalyst {
 				word = word.toLowerCase();
 				word = word.replaceAll("[^a-zA-Z]", "");
 				word = word.trim();
+				try {
+					word = stem(word);
+				}
+				catch (Throwable e) {
+					System.out.println("Error in stemming algorithm.");
+					return;
+				}
 				
 				if (wordCatalog.containsKey(word)) {
 					int value = wordCatalog.get(word) + 1;
@@ -39,106 +67,164 @@ public class FrequencyAnalyst {
 					wordCatalog.put(word, 1);
 				}
 			}
+			activeData = true;
 		}
 		catch (FileNotFoundException e) {
-			System.out.println("Failed to open file.");
+			System.out.println("Failed to open file: " + filepath);
 		}
 	}
 	
 	public float mean() {
-		int numWords = 0;
-		float total = 0;
-		
-		Set<String> words = wordCatalog.keySet();
-		
-		for (String word : words) {
-			total += wordCatalog.get(word);
-			numWords++;
-		}
-		
-		return total / numWords;
-	}
-	
-	public int frequency(String word) {
-		if (contains(word))
-			return wordCatalog.get(word);
-		else
-			return 0;
-	}
-	
-	public boolean contains(String word) {
-		return (wordCatalog.get(word) != null);
-	}
-	
-	public int numWords() {
-		return wordCatalog.size();
-	}
-	
-	public int min() {
-		int min = Integer.MAX_VALUE;
-		
-		Set<String> words = wordCatalog.keySet();
-		for (String word : words) {
-			if (wordCatalog.get(word) < min) {
-				min = wordCatalog.get(word);
-			}
-		}
-		
-		return min;
-	}
-	
-	public int max() {
-		int max = Integer.MIN_VALUE;
-		
-		Set<String> words = wordCatalog.keySet();
-		for (String word : words) {
-			if (wordCatalog.get(word) > max) {
-				max = wordCatalog.get(word);
-			}
-		}
-		
-		return max;
-	}
-	
-	public float sd() {
-		int[] frequencies = new int[wordCatalog.size()];
-		Set<String> words = wordCatalog.keySet();
-		int counter = 0;
-		for (String word : words) {
-			frequencies[counter] = wordCatalog.get(word);
-			counter++;
-		}
-		
-		float mean = mean();
-		
-		float sum = 0;
-		for (int i = 0; i < counter; ++i) {
-			sum += Math.pow(frequencies[i] - mean, 2);
-		}
-		double omega = Math.sqrt(sum / counter);
-		
-		return (float) omega;
-	}
-	
-	public boolean writeToFile(String directory, String fname) {
-		try {
-			PrintWriter writer = new PrintWriter(directory + "/" + fname);
+		if (activeData) {
+			int numWords = 0;
+			float total = 0;
+			
 			Set<String> words = wordCatalog.keySet();
 			
 			for (String word : words) {
-				if (word.trim().length() > 0) {
-					writer.println(String.format("%-" + 40 + "s", word) + "\t" + wordCatalog.get(word));
+				total += wordCatalog.get(word);
+				numWords++;
+			}
+			
+			return total / numWords;
+		}
+		else {
+			throw new RuntimeException("No valid data to analyze.");
+		}
+	}
+	
+	public int frequency(String word) {
+		if (activeData) {
+			if (contains(word))
+				return wordCatalog.get(word);
+			else
+				return 0;
+		}
+		else throw new RuntimeException("No valid data to analyze.");
+	}
+	
+	public boolean contains(String word) {
+		if (activeData) {
+			return (wordCatalog.get(word) != null);
+		}
+		else throw new RuntimeException("No valid data to analyze.");
+	}
+	
+	public int numWords() {
+		if (activeData) return wordCatalog.size();
+		
+		else throw new RuntimeException("No valid data to analyze.");
+	}
+	
+	public int min() {
+		if (activeData) {
+			int min = Integer.MAX_VALUE;
+			
+			Set<String> words = wordCatalog.keySet();
+			for (String word : words) {
+				if (wordCatalog.get(word) < min) {
+					min = wordCatalog.get(word);
 				}
 			}
 			
-			writer.close();
-			return true;
+			return min;
 		}
-		catch (FileNotFoundException fnfe) {
-			return false;
+		else throw new RuntimeException("No valid data to analyze.");
+	}
+	
+	public int max() {
+		if (activeData) {
+			int max = Integer.MIN_VALUE;
+			
+			Set<String> words = wordCatalog.keySet();
+			for (String word : words) {
+				if (wordCatalog.get(word) > max) {
+					max = wordCatalog.get(word);
+				}
+			}
+			
+			return max;
 		}
-		catch (SecurityException se) {
-			return false;
+		else throw new RuntimeException("No valid data to analyze.");
+	}
+	
+	public float sd() {
+		if (activeData) {
+			int[] frequencies = new int[wordCatalog.size()];
+			Set<String> words = wordCatalog.keySet();
+			int counter = 0;
+			for (String word : words) {
+				frequencies[counter] = wordCatalog.get(word);
+				counter++;
+			}
+			
+			float mean = mean();
+			
+			float sum = 0;
+			for (int i = 0; i < counter; ++i) {
+				sum += Math.pow(frequencies[i] - mean, 2);
+			}
+			double omega = Math.sqrt(sum / counter);
+			
+			return (float) omega;
+		}
+		else throw new RuntimeException("No valid data to analyze.");
+	}
+	
+	public boolean writeToFile(String directory, String fname) {
+		if (activeData) {
+			try {				
+				PrintWriter writer = new PrintWriter(directory + "/" + fname);
+				Set<String> words = wordCatalog.keySet();
+				
+				JSONObject freqAn = new JSONObject();
+				
+				JSONObject word = new JSONObject();
+				JSONArray wordLib = new JSONArray();
+				ArrayList<JSONObject> wordVector = new ArrayList<JSONObject>();
+				
+				for (String w : words) {
+					if (w.trim().length() > 0) {
+						wordVector.add(new JSONObject());
+						
+						wordVector.get(wordVector.size() -1).put("word", w);
+						wordVector.get(wordVector.size() - 1).put("frequency",  wordCatalog.get(w));
+						wordVector.get(wordVector.size() - 1).put("difficulty_level", "TBD");
+						
+						wordLib.add(wordVector.get(wordVector.size() - 1));
+					}
+				}
+				freqAn.put("words", wordLib);
+				
+				freqAn.put("total_num_words", numWords());
+				freqAn.put("standard_deviation", sd());
+				freqAn.put("mean_word_frequency", mean());
+				freqAn.put("text_name", fname.replaceAll(".json", ""));
+				
+				writer.print(freqAn.toJSONString());
+				writer.close();
+				return true;
+			}
+			catch (FileNotFoundException fnfe) {
+				return false;
+			}
+			catch (SecurityException se) {
+				return false;
+			}
+		}
+	else throw new RuntimeException("No valid data to analyze.");
+	}
+	
+	public void readAllDocuments(String dirpath) {
+		File dir = new File(dirpath);
+		File[] directoryListing = dir.listFiles();
+			
+		for (File text : directoryListing) {
+			if (text.getName().contains(".txt")) {
+				readDocument(text.getAbsolutePath());
+			}
 		}
 	}
 }
+
+	
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 public class Main {
 
 	public static void main(String[] args) {
-		String filepath = "/Users/schatridge/Downloads/books_for_clay";
+		String filepath = args[0];
 		FrequencyAnalyst analyzer = new FrequencyAnalyst();
 		
 		File dir = new File(filepath);
@@ -14,10 +14,16 @@ public class Main {
 				if (child.getName().contains(".txt")) {
 					analyzer.clearAndReadDocument(filepath + "/" + child.getName());
 					System.out.println(analyzer.frequency("fun"));
+					
+					String fname = child.getName().replaceAll(".txt", ".json");
 				
-					analyzer.writeToFile("/Users/schatridge/Downloads/books_for_clay/sorted", child.getName());
+					analyzer.writeToFile(args[1], fname);
 		        	}
-				}
+			}
+			
+			analyzer.clear();
+			analyzer.readAllDocuments(args[0]);
+			analyzer.writeToFile(args[1], "allTexts.json");
 		}
 		else
 			System.out.println("Failure");
